4fc750375a463e1e19a8ce14646ea60555283806
jahongirka178 <jahongirka178@gmail.com>
2025-07-23 19:36:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,9 @@ for name, model in models.items():
     model.fit(X_train_encoded, y_train)
     acc_train = accuracy_score(y_train, model.predict(X_train_encoded))
     acc_test =  accuracy_score(y_test, model.predict(X_test_endcoded))
+    st.write(f'{acc_train = }')
+    st.write(f'{acc_test = }')
+
 
 
 
