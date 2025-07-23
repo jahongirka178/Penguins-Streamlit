e188261861c5670f091e6d9f3bacdb86799f3c9f
jahongirka178 <jahongirka178@gmail.com>
2025-07-23 19:24:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ y = df['species']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-st.write('После split')
 
+encoder = ce.TargetEncoder(cols=['island','sex'])
+X_train_encoded = encoder.fit_transoform(X_train, y_train)
+X_test_encoded = encoder.transform(X_test)
 
+st.write('After encoding')
