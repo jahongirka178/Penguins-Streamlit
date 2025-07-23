4ea0bd449f8df3ff6baf827746900022fd36c5e9
jahongirka178 <jahongirka178@gmail.com>
2025-07-23 18:44:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Ссылка на raw CSV-файл в GitHub
 url = "https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv"
 
-df=pf.read_csv(url)
+df=pd.read_csv(url)
 
 st.write("Данные из GitHub:")
 st.dataframe(df)
