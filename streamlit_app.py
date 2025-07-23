585bd3206beacdfe51b310a3a75a6673ed162672
jahongirka178 <jahongirka178@gmail.com>
2025-07-23 19:00:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import category_encoders as ce
 import plotly.express as px
 
 url = "https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv"
