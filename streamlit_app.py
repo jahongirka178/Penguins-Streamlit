7bfb039fd88a353fc7b0a2c71d1d07754fb1f879
jahongirka178 <jahongirka178@gmail.com>
2025-07-23 18:51:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,19 @@ df=pd.read_csv(url)
 
 st.write("Данные из GitHub:")
 st.dataframe(df)
+
+
+x_axis = st.selectbox("Выберите переменную по оси X", df.select_dtypes(include='number').columns)
+y_axis = st.selectbox("Выберите переменную по оси Y", df.select_dtypes(include='number').columns, index=1)
+
+# Построение графика
+fig = px.scatter(
+    df,
+    x=x_axis,
+    y=y_axis,
+    color="species",
+    symbol="sex",
+    title="Визуализация данных пингвинов"
+)
+
+st.plotly_chart(fig)
